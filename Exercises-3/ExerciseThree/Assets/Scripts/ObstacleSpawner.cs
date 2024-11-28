a2d1cf2a8dcd3f25efca3889978a452a0d0c34bd
C-bridson <Cameronbridson13@aol.com>
2024-11-28 00:31:07 +0000

    Adapted the obstacles script to use pool technique
    using the pooling technique to reduce lag for replay ability
    TO DO
    add restart button

--- Exercises-3/ExerciseThree/Assets/Scripts/ObstacleSpawner.cs
+++ Exercises-3/ExerciseThree/Assets/Scripts/ObstacleSpawner.cs
@@ -7,8 +7,13 @@ public class ObstacleSpawner : MonoBehaviour
 {
     [SerializeField]
     private GameObject obstaclePrefab;
+
+
+    // pool size
     [SerializeField]
     private int obstacleMax;
+
+    private List<GameObject> obstaclesPool = new List<GameObject>();
 
 
     /// <summary>
@@ -35,26 +40,55 @@ public class ObstacleSpawner : MonoBehaviour
     private GameObject obstacleFolder;
 
 
-    private void Awake()
-    {
-        obstacleFolder = GameObject.FindWithTag("Obstacle");
-    }
-
     /// <summary>
     /// taking from the range and the prefab we iterate instantiations of the Obstacle, to a set max. 
     /// we set their parent to a blank object to act as a folder.
+    /// 
+    /// deactivate them
+    /// and add to pool
     /// </summary>
-    private void Start()
+    private void Awake()
     {
-  
+        obstacleFolder = GameObject.FindWithTag("Obstacle");
+
         for (int i = 0; i < obstacleMax; i++)
         {
-            GameObject temp = Instantiate(obstaclePrefab, new Vector3(Random.Range(obstacleArea.min, obstacleArea.max), 0.5f, Random.Range(obstacleArea.min, obstacleArea.max)), Quaternion.identity);
+            GameObject temp = Instantiate(obstaclePrefab, GetRandomPos(), Quaternion.identity) ;
             temp.transform.parent = obstacleFolder.transform;
+
+            temp.SetActive(true);
+            obstaclesPool.Add(temp);
+
         }
+
+    }
+
+    /// <summary>
+    /// random positions generator based on set parameters 
+    /// </summary>
+    /// <returns></returns>
+    private Vector3 GetRandomPos()
+    {
+        return new Vector3(Random.Range(obstacleArea.min, obstacleArea.max), 0.5f, Random.Range(obstacleArea.min, obstacleArea.max));
     }
 
 
+    /// <summary>
+    /// loops through List and switches objects off and on, while changing their positions 
+    /// </summary>
+    public void ResetObstaclePool()
+    {
+        foreach (var item in obstaclesPool)
+        {
+            if (item != null)
+            {
+                item.SetActive(false);
+                item.transform.position = GetRandomPos();
+                item.SetActive(true);
+            }
+        }
 
+
+    }
 
 }
